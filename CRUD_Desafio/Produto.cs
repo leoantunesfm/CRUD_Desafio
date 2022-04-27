90be0af461b00a0813d69fbee9f3ba42b07870a6
Leandro Machado <leoantunes.fm@gmail.com>
2022-04-26 22:44:46 -0300

    Commit inicial -- Criação do projeto e classe Produto.cs

---
+++ CRUD_Desafio/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_Desafio
+{
+    class Produto
+    {
+        public int Codigo { get; private set; }
+        public string Descricao { get; private set; }
+        public double Preco { get; private set; }
+        public int QtdEstoque { get; private set; }
+
+        public Produto(int codigo, string descricao)
+        {
+            Codigo = codigo;
+            Descricao = descricao;
+        }
+
+        public void SetPreco (double valor)
+        {
+            Preco = valor;
+        }
+    }
+}
---
+++ CRUD_Desafio/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUD_Desafio
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de Produtos");
+        }
+    }
+}
